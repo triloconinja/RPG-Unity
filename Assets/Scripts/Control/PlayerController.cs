578290b0fd002699961665c2787ada5901829a0d
Maybank Pro 2017 <maybankpro2017@Maybanks-MacBook-Pro.local>
2019-08-05 16:30:50 +0800

    Combat

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,3 +1,5 @@
+using System;
+using RPG.Combat;
 using RPG.Movement;
 using UnityEngine;
 namespace RPG.Control
@@ -7,6 +9,27 @@ namespace RPG.Control
 
         private void Update()
         {
+            InteractWithCombat();
+            InteractWithMovement();
+        }
+
+        private void InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach(RaycastHit hit in hits){
+               CombatTarget target =  hit.transform.GetComponent<CombatTarget>();
+               if(target == null) continue;
+
+                if(Input.GetMouseButton(0))
+                {
+                    GetComponent<Fighter>().Attack(target);
+                }
+
+            }
+        }
+
+        private void InteractWithMovement()
+        {
             if (Input.GetMouseButton(0))
             {
                 MoveToCursor();
@@ -15,13 +38,17 @@ namespace RPG.Control
 
         private void MoveToCursor()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            bool hasHit = Physics.Raycast(ray, out hit);
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
             if (hasHit)
             {
                 GetComponent<Mover>().MoveTo(hit.point);
             }
+        }
+
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -4,23 +4,18 @@ using UnityEngine;
 using UnityEngine.AI;
 namespace RPG.Movement
 {
-
-
     public class Mover : MonoBehaviour
     {
         [SerializeField] Transform target;
 
         void Update()
         {
-
             UpdateAnimator();
         }
 
         public void MoveTo(Vector3 destination)
         {
-            {
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); GetComponent<NavMeshAgent>().destination = destination;
-            }
+            GetComponent<NavMeshAgent>().destination = destination;
         }
 
         private void UpdateAnimator()
@@ -28,7 +23,7 @@ namespace RPG.Movement
             Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
             Vector3 localVelocity = transform.InverseTransformDirection(velocity);
             float speed = localVelocity.z;
-            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);   
         }
     }
 }
